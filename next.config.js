7b49e31c56da5728dbb6f6ef0653df310c7269b2
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-01 19:59:01 +0530

    feat: content layer setup completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const { withContentlayer } = require('next-contentlayer')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true }
+
+module.exports = withContentlayer(nextConfig)
