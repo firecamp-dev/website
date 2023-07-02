2d6a71c5b84bf33d7402bc2d359e0b14e46322e0
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-02 12:05:37 +0530

    feat: blog setup in-progress

--- env.mjs
+++ env.mjs
@@ -6,9 +6,9 @@ export const env = createEnv({
     // GITHUB_CLIENT_ID: z.string().min(1),
   },
   client: {
-    // NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string(),
   },
   runtimeEnv: {
-    // NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
