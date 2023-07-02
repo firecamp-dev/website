524a14415fb835502978dcb5751bbbf83ef60932
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-02 12:19:44 +0530

    feat: content layer config changes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,32 +1,70 @@
 import Link from 'next/link'
-import { compareDesc, format, parseISO } from 'date-fns'
 import { allPosts, Post } from 'contentlayer/generated'
 
-function PostCard(post: Post) {
-  return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
-    </div>
-  )
+import Image from "next/image"
+import { compareDesc } from "date-fns"
+
+import { formatDate } from "@/lib/utils"
+
+export const metadata = {
+  title: "Blog",
 }
 
-export default function Home() {
-  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+export default async function BlogPage() {
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date))
+    })
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
+            Blog
+          </h1>
+          <p className="text-xl text-muted-foreground">
+            A blog built using Contentlayer. Posts are written in MDX.
+          </p>
+        </div>
+      </div>
+      <hr className="my-8" />
+      {posts?.length ? (
+        <div className="grid gap-10 sm:grid-cols-2">
+          {posts.map((post, index) => (
+            <article
+              key={post._id}
+              className="group relative flex flex-col space-y-2"
+            >
+              {post.image && (
+                <Image
+                  src={post.image}
+                  alt={post.title}
+                  width={804}
+                  height={452}
+                  className="rounded-md border bg-muted transition-colors"
+                  priority={index <= 1}
+                />
+              )}
+              <h2 className="text-2xl font-extrabold">{post.title}</h2>
+              {post.description && (
+                <p className="text-muted-foreground">{post.description}</p>
+              )}
+              {post.date && (
+                <p className="text-sm text-muted-foreground">
+                  {formatDate(post.date)}
+                </p>
+              )}
+              <Link href={post.url} className="absolute inset-0">
+                <span className="sr-only">View Article</span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>{posts.length} No posts published.</p>
+      )}
     </div>
   )
 }
--- posts/launching-firecamp.md
+++ posts/launching-firecamp.md
@@ -1,6 +1,8 @@
 ---
 title: My First Post
 date: 2023-12-24
+description: "this is the desc"
+slug: "first-blog"
 ---
 
 Ullamco et nostrud magna commodo nostrud ...
