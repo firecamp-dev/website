1bb882df1d1e9352d6bd90369c8286e84e8dca59
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-04 22:19:49 +0530

    feat: icons added

--- components/customer-section.tsx
+++ components/customer-section.tsx
@@ -10,36 +10,36 @@ export function CustomerSection() {
                 <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                     <Image
                         className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-                        alt="Transistor"
-                        width={158}
-                        height={48}
-                    />
-                    <Image
-                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-                        alt="Reform"
-                        width={158}
-                        height={48}
-                    />
-                    <Image
-                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-                        alt="Tuple"
+                        src="/images/users/alphasense.svg"
+                        alt="AlphaSense"
                         width={158}
                         height={48}
                     />
                     <Image
                         className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-                        alt="SavvyCal"
+                        src="/images/users/udaan.svg"
+                        alt="Udaan"
                         width={158}
                         height={48}
                     />
                     <Image
-                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-                        src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-                        alt="Statamic"
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="/images/users/accenture.svg"
+                        alt="Accenture"
+                        width={158}
+                        height={48}
+                    />
+                    <Image
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="/images/users/automattic.svg"
+                        alt="Automatic"
+                        width={158}
+                        height={48}
+                    />
+                    <Image
+                        className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                        src="/images/users/adobe.svg"
+                        alt="Adobe"
                         width={158}
                         height={48}
                     />
