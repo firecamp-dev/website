4849777e144ad32f97c9c6055dfe26b0b22194ad
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-04 17:09:19 +0530

    feat: cta change

--- components/cta-section.tsx
+++ components/cta-section.tsx
@@ -9,7 +9,7 @@ export function CtaSection() {
         <section id="cta" className="container py-8 md:py-12 lg:pb-40">
             <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
                 <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                    Pitch Your Tent at the API Campsite Today!
+                    Let the API Campsite be your home!
                 </h2>
                 <div className="space-x-4">
                     <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
