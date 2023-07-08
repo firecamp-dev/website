618b7299bb39ede1b43bbc00a320a8a06e292527
Nishchit Dhanani <dnishchit@gmail.com>
2023-07-08 18:59:35 +0530

    chore

--- config/site.ts
+++ config/site.ts
@@ -3,8 +3,8 @@ import { SiteConfig } from "types"
 export const siteConfig: SiteConfig = {
   name: "Firecamp",
   description: "Open Source Postman Alternative",
-  url: "https://firexamp.io",
-  ogImage: "https://firecamp.io/og.jpg",
+  url: "/", //"https://firexamp.io",
+  ogImage: "/og.png",
   links: {
     platform: "https://firecamp.dev",
     twitter: "https://twitter.com/FirecampDev",
