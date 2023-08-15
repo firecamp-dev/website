fd44762e44e088add1ca6850fb4ff54ba0c18ed9
Nishchit Dhanani <dnishchit@gmail.com>
2023-08-15 22:39:31 +0530

    chore: github link enable

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -9,11 +9,12 @@ export function HeroFSection() {
         <section className="space-y-10 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-44">
             <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
                 <Link
-                    href={siteConfig.links.twitter}
+                    href={siteConfig.links.github}
                     className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
                     target="_blank"
                 >
-                    Follow along on Twitter
+                    Star us on Github
+                    {/* Follow along on Twitter */}
                 </Link>
                 <h1 className="font-heading text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-slate-500 to-slate-900 bg-clip-text text-transparent">
                     Open Source Postman Alternative
