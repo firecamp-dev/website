f4c506c7d4033ce03a7e91484038183226ed8560
Nishchit Dhanani <dnishchit@gmail.com>
2023-09-08 12:53:25 +0530

    removed www

--- config/seo.ts
+++ config/seo.ts
@@ -1,39 +1,39 @@
-const image = "https://www.firecamp.io/og.png";
+const image = "https://firecamp.io/og.png";
 export const Seo: any = {
     "home": {
         "title": "Firecamp - Get instant API playgrounds to test Rest, GraphQL, WebSocket and SocketIO APIs",
         "description": "Firecamp is an Open Source API platform for testing and developing Multi-protocol APIs like Rest, GraphQL, WebSocket, SocketIO, and others. It's a secure, cost-effective and OpenSource Postman alternative for teams.",
-        "url": "https://www.firecamp.io/",
+        "url": "https://firecamp.io",
         image,
         "imageAlt": "API DevTool",
-        "siteName": "https://www.firecamp.io/",
+        "siteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     },
     "pricing": {
         "title": "Flexible pricing | Firecamp",
         "description": "Discover the best Firecamp pricing plans tailored to suit your development requirements. Explore our cost-effective options and select the ideal plan for your team. Firecamp is 3x affordable than other alternative like postman in pricing",
-        "url": "https://www.firecamp.io/pricing",
+        "url": "https://firecamp.io/pricing",
         image,
         "imageAlt": "Firecamp Pricing",
-        "siteName": "https://www.firecamp.io/",
+        "siteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     },
     "rest": {
         "title": "Minimal Rest API client for your team | Firecamp",
         "description": "Firecamp is a minimalist Rest API client that facilitates faster API building with features like workspace, API collection, requests, and more. It allows for collaborative API development with your team and serves as an open-source alternative to Postman.",
-        "ogUrl": "https://www.firecamp.io/rest",
+        "ogUrl": "https://firecamp.io/rest",
         image,
         "ogImageAlt": "",
-        "siteName": "https://www.firecamp.io/",
+        "siteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     },
     "graphql": {
         "title": "Collaborative GraphQL Playground for your team | Firecamp",
         "description": "Firecamp offers seamless GraphQL operations with our collaborative GraphQL Playground. Streamline your workflow and collaborate with your team to prepare, share, and export your query collection. Apollo studio alternative.",
-        "ogUrl": "https://www.firecamp.io/graphql",
+        "ogUrl": "https://firecamp.io/graphql",
         image,
         "ogImageAlt": "",
-        "ogSiteName": "https://www.firecamp.io/",
+        "ogSiteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     },
     "websocket": {
@@ -42,16 +42,16 @@ export const Seo: any = {
         "ogUrl": "https://firecamp.io/websocket",
         image,
         "ogImageAlt": "",
-        "ogSiteName": "https://www.firecamp.io/",
+        "ogSiteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     },
     "socketio": {
         "title": "Collaborative SocketIO Playground for your team | Firecamp",
         "description": "Firecamp SocketIO client offers comprehensive solution for testing, debugging, and visualizing SocketIO emitters, listeners and events, online socket-io tester, socket-io testing, postman for socket-io",
-        "ogUrl": "https://www.firecamp.io/socketio",
+        "ogUrl": "https://firecamp.io/socketio",
         image,
         "ogImageAlt": "",
-        "ogSiteName": "https://www.firecamp.io/",
+        "ogSiteName": "https://firecamp.io",
         "creatorName": "@firecampdev"
     }
 }
