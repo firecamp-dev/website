9df58facb815727bae5442dfd84203248e8a5a6e
Nishchit <dnishchit@gmail.com>
2023-10-19 12:34:50 +0530

    Create GAnalytics.tsx

---
+++ components/GAnalytics.tsx
@@ -0,0 +1,20 @@
+import Script from 'next/script'
+ 
+function Home() {
+  return (
+    <div className="container">
+      <Script src=`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}` />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
+        `}
+      </Script>
+    </div>
+  )
+}
+ 
+export default Home
