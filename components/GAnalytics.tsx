94f989059a5637ec962381b15d52af7cfadf2c7e
Nishchit Dhanani <dnishchit@gmail.com>
2023-10-19 12:51:40 +0530

    fix: script issue fixed

--- components/GAnalytics.tsx
+++ components/GAnalytics.tsx
@@ -9,7 +9,7 @@ export function GAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS);
+          gtag('config', '{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
     </div>
