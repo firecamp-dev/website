4cb006ab7f0a9f52440bfb25bc6ddf21a420a393
Mosaab Emam <mosaab.emam123@gmail.com>
2023-10-23 21:55:27 +0200

    Update slate values

--- components/HeroHeading.tsx
+++ components/HeroHeading.tsx
@@ -6,7 +6,7 @@ export default function HeroHeading() {
     const { theme } = useTheme();
 
     return (
-        <h1 className={`font-heading text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r  bg-clip-text text-transparent ${theme == "light" ? "from-slate-500 to-slate-900" : " from-amber-200 to-orange-400"}`}>
+        <h1 className={`font-heading text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r  bg-clip-text text-transparent ${theme == "light" ? "from-slate-400 to-slate-700" : " from-amber-200 to-orange-400"}`}>
             Open Source Postman Alternative
         </h1>
     );
