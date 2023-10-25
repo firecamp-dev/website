98c6022b8ae6a53581ea1d6adb24a2dc93c74e94
Mosaab Emam <mosaab.emam123@gmail.com>
2023-10-25 18:18:10 +0200

    Fixing default color values

--- components/HeroHeading.tsx
+++ components/HeroHeading.tsx
@@ -6,7 +6,7 @@ export default function HeroHeading() {
     const { theme } = useTheme();
 
     return (
-        <h1 className={`font-heading text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r  bg-clip-text text-transparent ${theme == "light" ? "from-slate-400 to-slate-700" : " from-amber-200 to-orange-400"}`}>
+        <h1 className={`font-heading text-3xl tracking-tight font-extrabold sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r  bg-clip-text text-transparent ${theme == "dark" ? " from-amber-200 to-orange-400" : " from-slate-400 to-slate-700"}`}>
             Open Source Postman Alternative
         </h1>
     );
